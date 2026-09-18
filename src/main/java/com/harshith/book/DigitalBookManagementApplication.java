5a03fb7959bd438897236979526079e6a55e3915
M SAI HARSHITH GOUD <166594237+thenameisharshith@users.noreply.github.com>
2026-09-18 11:21:31 +0530

    Add digital book entities repositories and exception handling

---
+++ src/main/java/com/harshith/book/DigitalBookManagementApplication.java
@@ -0,0 +1,7 @@
+package com.harshith.book;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DigitalBookManagementApplication {
+ public static void main(String[] args){SpringApplication.run(DigitalBookManagementApplication.class,args);}
+}
